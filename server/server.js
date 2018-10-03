095861dffaef4d86d0e52ef35b37b721536ec4e9
dhantha <dhantha_gunarathna@comcast.com>
2018-10-03 00:35:44 -0400

    Add the emmiters

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO().listen(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-})
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('createMessege', (messege) => {
+    console.log('create messege', messege);
+  });
+
+  socket.emit('newMessege', {
+    from: 'kevin',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnect from the client');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
